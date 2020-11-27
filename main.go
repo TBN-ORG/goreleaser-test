da1221f943bcfae50f7516eb901de2b60a80ff06
rfinner <rfinneran@tbn.tv>
2020-11-26 19:16:05 -0800

    updates to test

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 // main.go
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/table"
+)
+
 func main() {
 	println("Ba dum, tss!")
+	fmt.Println("Do Dah")
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Network", "Start Time", "Media ID", "Title"})
+	t.AppendRow([]interface{}{"TBN", "now", "M1001", "Praise"})
+	t.AppendFooter(table.Row{"", "", "Events Count", 1})
+	t.SetStyle(table.StyleColoredBlackOnBlueWhite)
+	t.Render()
 }
